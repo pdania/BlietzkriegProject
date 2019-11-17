448673e72f3a3474622144a4f876c1454bc315dd
Danylo Parkhomenko <pdania@ukr.net>
2019-11-17 18:17:51 +0200

    Redesined ScheduledTransaction

--- BlietzkriegProject/MainPage.xaml.cs
+++ BlietzkriegProject/MainPage.xaml.cs
@@ -23,7 +23,7 @@ namespace UI
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 //            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(500,320));
             NavigationManager.Instance.Initialize(new InitializationNavigationModel(this));
-            NavigationManager.Instance.Navigate(ViewType.Login);
+            NavigationManager.Instance.Navigate(ViewType.ScheduledTransaction);
         }
 
         public ContentControl ContentControl
